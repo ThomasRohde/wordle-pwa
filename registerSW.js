de14982488cea5caaffe54fc91a18973765748ad
Thomas Klok Rohde <thomas@rohde.name>
2025-05-24 09:13:06 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wordle-pwa/sw.js', { scope: '/wordle-pwa/' })})}
